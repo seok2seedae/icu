588cc0ce070ca5035f4953ea864df254b2021c13
Your Name <you@example.com>
2024-01-15 14:20:00 +0900

    Re add icu module

---
+++ README.md
@@ -0,0 +1,2 @@
+# Git_One
+# icu
---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// 미들웨어
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening http://localhost:${PORT}`);
+});
+
+module.exports = {
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('cssnano')({
+      preset: 'default',
+    }),
+  ],
+}
