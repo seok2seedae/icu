85938756ded66fa06d0179d0f6b33c259b5a29fa
Your Name <you@example.com>
2024-01-15 15:55:15 +0900

    base set

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3333;
 
 // 미들웨어
 app.use(express.static(path.join(__dirname, 'public')));
